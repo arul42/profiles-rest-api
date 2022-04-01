6af80643d1edeb1ec5dfe7f2f52f248ae614ba5e
devopskalyani21 <devopskalyani21@gmail.com>
2022-04-01 09:56:45 +0530

    deployemtn

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/arul42/Python-Django.git'
+PROJECT_GIT_URL='https://github.com/arul42/profiles-rest-api.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
